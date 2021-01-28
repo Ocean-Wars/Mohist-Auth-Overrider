a5db30be44b49e58ae6bd5bc48c8a0994ec0aa58
Nicolas Went <nicolas.went@epita.fr>
2021-01-28 13:58:53 +0100

    [Archi] First commit

---
+++ src/fr/oceanwars/authoverrider/AuthOverrider.java
@@ -0,0 +1,15 @@
+package fr.oceanwars.authoverrider;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AuthOverrider extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("HELLO");
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
